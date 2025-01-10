64443dba68793e22f6ee9cb05b99ce8bb57b3502
timnasa999 <Timotheomgaya@gmail.com>
2025-01-10 19:26:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+
+ 
+## 𝐅𝐎𝐑 𝐎𝐍𝐄-𝐓𝐀𝐏 𝐃𝐄𝐏𝐋𝐎𝐘𝐌𝐄𝐍𝐓 𝐔𝐒𝐄 𝐓𝐇𝐈𝐒 𝐁𝐔𝐓𝐓𝐎𝐍
+
+   🕳IF YOU DON'T HAVE A HEROKU ACCOUNT...CREATE ONE
+   
+   <a href="https://signup.heroku.com/"><img title="CREATE-ACCOUNT" src="https://img.shields.io/badge/CREATE-ACCOUNT-h?color=blue&style=for-the-badge&logo=blue" width="180" height="43.45"/></a></p>
+
+   ☢️IF YOU ALREADY HAVE A HEROKU ACCOUNT...DEPLOY NOW
+
+ <a href="https://dashboard.heroku.com/new?template=https://github.com/NjabuloJ/Njabulo-JB1"><img title="DEPLOY-ON HEROKU" src="https://img.shields.io/badge/DEPLOY-ON HEROKU-h?color=blue&style=for-the-badge&logo=nike" width="220" height="38.45"/></a></p>
+
+ 
+ [![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Rockstar-ExtraBold&size=30&pause=1000&color=0000FF&center=true&vCenter=true&width=815&height=60&lines=▭+▬+▭+▬+▭+▬+▭+▬+▭+▬+▭)](https://git.io/typing-svg) 
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+## FINAL REMARKS ON MY REPO (STATS)
+
+![ Stats](https://github-readme-stats.vercel.app/api/pin/?username=Enzotech&repo=ENZO-MD&show_owner=true&theme=dark)
+
+
+
+
+
+
+
+
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+## CONTACT 
+  DM FOR SERIOUS BUSINESS
+
+   <a href="https://github.com/NjabuloJ/NjabuloJ-INFO"><img title="CONTACT-Njabulo" src="https://img.shields.io/badge/CONTACT-Njabulo-h?color=black&style=for-the-badge&logo=audi" width="240" height="45.45"/></a></p>
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+## 🚀☆ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴏғғɪᴄᴇ☂︎📃🚀
---
+++ commandes/repo.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "🔎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/NjabuloJ/Njabulo-jb';
+  const img = 'https://files.catbox.moe/xfn913.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `> *Hi there,If you need Njabulo-Jb repo*\n> *Anything You want Is Here*
+╭─────────────────
+││ *𝐒𝐞𝐬𝐬𝐢𝐨𝐧* https://shorturl.at/9WfYs
+││ *𝐑𝐞𝐩𝐨:* https://shorturl.at/0TtVa
+││ *𝐒𝐭𝐚𝐫𝐬:* ${repoInfo.stars}
+││ *𝐅𝐨𝐫𝐤𝐬:* ${repoInfo.forks}
+││ *𝐑𝐞𝐥𝐞𝐚𝐬𝐞 𝐃𝐚𝐭𝐞:* ${releaseDate}
+││ *𝐔𝐩𝐝𝐚𝐭𝐞𝐝:* ${repoInfo.lastUpdate}
+││ *𝐎𝐰𝐧𝐞𝐫:* *Njabulo*
+││ *𝐂𝐡𝐚𝐧𝐧𝐞𝐥:* https://shorturl.at/q8ZuS
+╰─────────────────
+> *Njabulo Jb*`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
